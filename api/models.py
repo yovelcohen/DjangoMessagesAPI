6a92e184c6ec2fdde736eed2ea863ef5e4d10e54
yovelcohen <yovell04@gmail.com>
2020-06-12 17:05:16 +0300

    Fixed JWT hashing

--- api/models.py
+++ api/models.py
@@ -1,9 +1,13 @@
+from datetime import datetime, timedelta
+
+import jwt
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
 from .Utils.Consts import MessageFields
+from .Utils.managers import UserManager
 from .Utils.validators import validate_message_content
 
 
@@ -29,4 +33,57 @@ class Message(models.Model):
 
 
 class User(AbstractUser):
-    pass
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    is_staff = models.BooleanField(default=False)
+
+    # A timestamp representing when this object was created.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # A timestamp reprensenting when this object was last updated.
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    # Tells Django that the UserManager class defined above should manage
+    # objects of this type.
+    objects = UserManager()
+
+    def __str__(self):
+        """
+        Returns a string representation of this `User`.
+
+        This string is used when a `User` is printed in the console.
+        """
+        return self.email
+
+    @property
+    def token(self):
+        """
+        Allows us to get a user's token by calling `user.token` instead of
+        `user.generate_jwt_token().
+        """
+        return self._generate_jwt_token()
+
+    def get_full_name(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.username
+
+    def _generate_jwt_token(self):
+        """
+        Generates a JSON Web Token that stores this user's ID and has an expiry
+        date set to 60 days into the future.
+        """
+        dt = datetime.now() + timedelta(days=60)
+
+        token = jwt.encode({
+            'id': self.pk,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, algorithm='HS256')
+
+        return token.decode('utf-8')
--- api/views.py
+++ api/views.py
@@ -1,20 +1,18 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status
-from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from .Serializers.message_serializer import MessageSerializer
-from .Serializers.user_serializer import UserSerializer
 from .Utils.Consts import MessageFields
 from .models import Message
 
 
 class MessagesViewSet(ModelViewSet):
     """
-    A simple ViewSet for viewing and editing the accounts
+    A simple ViewSet for viewing and editing the messages
     associated with the user.
     """
     authentication_classes = [JSONWebTokenAuthentication, ]
@@ -55,25 +53,3 @@ class MessagesViewSet(ModelViewSet):
         data = self.filter_queryset(self.get_queryset())
         serialized_data = MessageSerializer(data, many=True)
         return Response(serialized_data.data, status=HTTP_200_OK)
-
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-
-class UserCreate(APIView):
-    """
-    Creates the user.
-    """
-
-    def post(self, request, format='json'):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            if user:
-                token = Token.objects.create(user=user)
-                json = serializer.data
-                json['token'] = token.key
-                return Response(json, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
