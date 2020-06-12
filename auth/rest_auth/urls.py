80d6c2185c535ec34acb41d3f35cec6563794a2d
yovelcohen <yovell04@gmail.com>
2020-06-12 16:35:20 +0300

    added token verify view

--- auth/rest_auth/urls.py
+++ auth/rest_auth/urls.py
@@ -5,5 +5,6 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
     path('auth/sign-up/', include('rest_auth.registration.urls')),
 ]
