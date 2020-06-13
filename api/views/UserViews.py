c7c90efd0ad8ac88ae078f780912ec5caf021497
yovelcohen <yovell04@gmail.com>
2020-06-13 18:02:10 +0300

    GenericViewSet

--- api/views/UserViews.py
+++ api/views/UserViews.py
@@ -3,29 +3,31 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import GenericViewSet
 
+from ..models import User
 from ..serializers.UserSerializers import UserSerializer
 from ..utils.ViewsConsts import DocsDescriptions
-from ..models import User
 
 
-class UserViewSet(ViewSet):
+class UserViewSet(GenericViewSet):
     """
     A simple ViewSet for listing or retrieving users.
     """
     authentication_classes = [TokenAuthentication, ]
     permission_classes = [IsAdminUser]
 
+    def get_queryset(self):
+        queryset = User.objects.all()
+        return queryset
+
     @swagger_auto_schema(operation_summary=DocsDescriptions.LIST_USERS)
     def list(self, request):
-        queryset = User.objects.all()
-        serializer = UserSerializer(queryset, many=True)
+        serializer = UserSerializer(self.queryset(), many=True)
         return Response(serializer.data)
 
     @swagger_auto_schema(operation_summary=DocsDescriptions.RETRIEVE_USER)
     def retrieve(self, request, pk=None):
-        queryset = User.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
+        user = get_object_or_404(self.queryset(), pk=pk)
         serializer = UserSerializer(user)
         return Response(serializer.data)
