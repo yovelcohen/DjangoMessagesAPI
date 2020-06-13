d8c835419fd6b204a5d97c3162f59c56fb4662eb
Yovel Cohen <55362840+yovelcohen@users.noreply.github.com>
2020-06-13 16:08:22 +0300

    Update DataBase.py

--- messagesapi/config/DataBase.py
+++ messagesapi/config/DataBase.py
@@ -1,6 +1,6 @@
 POSTGRESQL_CONNECTION: dict = {'engine': 'django.db.backends.postgresql',
                                "user": 'yovel',
-                               "password": 'HUCKFVIlavoy',
+                               "password": '*****',
                                "host": "127.0.0.1",
                                "port": "5432",
                                "database": 'messaging3'
